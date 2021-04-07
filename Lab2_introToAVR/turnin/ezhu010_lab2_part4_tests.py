e04d1fbaa601f8eb203b42257335bf865fcdb99a
Edward Zhu <ezhu010@wch136-24.cs.ucr.edu>
2021-04-07 09:43:42 -0700

    test

---
+++ Lab2_introToAVR/turnin/ezhu010_lab2_part4_tests.py
@@ -0,0 +1,38 @@
+# Array of tests to run (in order)
+# Each test contains
+#   description - 
+#   steps - A list of steps to perform, each step can have
+#       inputs - A list of tuples for the inputs to apply at that step
+#       *time - The time (in ms) to wait before continuing to the next step 
+#           and before checking expected values for this step. The time should be a multiple of
+#           the period of the system
+#       *iterations - The number of clock ticks to wait (periods)
+#       expected - The expected value at the end of this step (after the "time" has elapsed.) 
+#           If this value is incorrect the test will fail early before completing.
+#       * only one of these should be used
+#   expected - The expected output (as a list of tuples) at the end of this test
+# An example set of tests is shown below. It is important to note that these tests are not "unit tests" in 
+# that they are not ran in isolation but in the order shown and the state of the device is not reset or 
+# altered in between executions (unless preconditions are used).
+# Optionally you can add a set of "watch" variables these need to be global or static and may need
+# to be scoped at the function level (for static variables) if there are naming conflicts. The 
+# variables listed here will display everytime you hit (and stop at) a breakpoint
+
+tests = [ {'description': 'PINA: 0, PINB: 35, PINC: 85 => PORTC: 0x03',
+    'steps': [ {'inputs': [('PINA',0), ('PINB', 35), ('PINC', 85)],'iterations': 5 } ],
+    'expected': [('PORTD',0x1E)],
+        },
+
+
+	{'description': 'PINA: 0, PINB: 50, PINC: 50 => PORTC: 0x18',
+    'steps': [ {'inputs': [('PINA',0), ('PINB', 50), ('PINC', 50)],'iterations': 5 } ],
+    'expected': [('PORTD',0x18)],
+        },
+       {'description': 'PINA: 20, PINB: 50, PINC: 85 => PORTC: 0x25',
+    'steps': [ {'inputs': [('PINA',20), ('PINB', 50), ('PINC', 85)],'iterations': 5 } ],
+    'expected': [('PORTD',0x25)],
+        },
+
+  ]
+#watch = ['PORTB']
+
