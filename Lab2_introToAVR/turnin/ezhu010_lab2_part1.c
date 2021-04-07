a64aa5cf06de1032bb52f890b11b52f456c4c65d
Edward Zhu <ezhu010@wch136-24.cs.ucr.edu>
2021-04-06 22:39:01 -0700

    added lab 2 part 1

---
+++ Lab2_introToAVR/turnin/ezhu010_lab2_part1.c
@@ -0,0 +1,41 @@
+/*	Author: ezhu010
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+	DDRA = 0x00;
+	DDRB = 0x0F;
+	unsigned char temp = 0;
+ 
+	while(1){
+		
+	   temp = PINA;
+	   if(temp == 0){
+		PORTB = 0x00;
+	   }
+	   else if(temp == 1){
+		PORTB = 0x01;
+	  }
+	  else if(temp == 2){
+		PORTB = 0x00;
+	  }
+	  else if(temp == 3){
+		PORTB = 0x00;
+	  }
+
+	}
+	
+	return 0;
+
+
+}
