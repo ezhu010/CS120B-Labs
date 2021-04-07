375727ad203ccda67b6202a140590100e35e5ac4
Edward Zhu <ezhu010@wch136-24.cs.ucr.edu>
2021-04-06 23:14:49 -0700

    completed part 2

---
+++ Lab2_introToAVR/turnin/ezhu010_lab2_part2.c
@@ -0,0 +1,33 @@
+/*	Author: ezhu010
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+
+	DDRA = 0x00;
+	DDRC = 0xFF;
+	PORTA = 0xFF;
+	PORTC = 0x00;
+	
+	unsigned char cntavail = 0;
+	while(1){
+	   
+	   
+	   cntavail =!(PINA & 0x01) + !(PINA & 0x02) + !(PINA & 0x04) + !(PINA & 0x08);
+	   PORTC = cntavail; 
+
+	}
+	return 0;
+
+
+}
