4829076588eeb3686986d6aa42f3be1729015908
Edward Zhu <ezhu010@wch136-24.cs.ucr.edu>
2021-04-08 11:24:35 -0700

    test

---
+++ Lab3_Bit_Manipulation/Lab3_Bit_Manipulation/turnin/ezhu010_lab3_part5_tests.py
@@ -0,0 +1,49 @@
+
+
+
+
+
+
+
+
+
+
+
+#Array of tests to run (in order)
+# Each test contains
+#   description - 
+#   steps - A list of steps to perform, each step can have
+#       inputs - A list of tuples for the inputs to apply at that step
+#       *time - The time (in ms) to wait before continuing to the next step 
+#           and before checking expected values for this step. The time should be a multiple of
+#           the period of the system
+#       *iterations - The number of clock ticks to wait (periods)
+#       expected - The expected value at the end of this step (after the "time" has elapsed.) 
+#           If this value is incorrect the test will fail early before completing.
+#       * only one of these should be used
+#   expected - The expected output (as a list of tuples) at the end of this test
+# An example set of tests is shown below. It is important to note that these tests are not "unit tests" in 
+# that they are not ran in isolation but in the order shown and the state of the device is not reset or 
+# altered in between executions (unless preconditions are used).
+tests = [ {'description': 'PIND: 0x0A PINB: 0x01 => PORTB: 0x04',
+    'steps': [ {'inputs': [('PINB',0x01), ('PIND', 0x0A)], 'iterations': 5 } ],
+    'expected': [('PORTB',0x04)]
+        },
+
+        {'description': 'PIND: 0x46 PINB: 0x01 => PORTB: 0x02',
+    'steps': [ {'inputs': [('PIND',0x46), ('PINB', 0x01)], 'iterations': 5 } ],
+    'expected': [('PORTB',0x02)]
+        },
+
+        {'description': 'PIND: 0x80 PINB: 0x00 => PORTB: 0x02',
+    'steps': [ {'inputs': [('PIND',0x80), ('PINB', 0x00)], 'iterations': 5 } ],
+    'expected': [('PORTB',0x02)]
+        },
+
+        {'description': 'PIND: 0x00 PINB: 0x01 => PORTB: 0x00',
+    'steps': [ {'inputs': [('PIND',0x00), ('PINB', 0x01)], 'iterations': 5 } ],
+    'expected': [('PORTB',0x00)]
+        },
+
+  ]
+#watch = ['PORTB'] 
