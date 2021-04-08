81525f795544077eacbdad934147cf86a214dbd3
Edward Zhu <ezhu010@wch136-24.cs.ucr.edu>
2021-04-08 12:24:47 -0700

    test

--- Lab3_Bit_Manipulation/Lab3_Bit_Manipulation/turnin/ezhu010_lab3_part4_tests.py
+++ Lab3_Bit_Manipulation/Lab3_Bit_Manipulation/turnin/ezhu010_lab3_part4_tests.py
@@ -1,35 +1,23 @@
-/*	Author: ezhu010
- *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
- *	Exercise Description: [optional - include for your own benefit]
- *
- *	I acknowledge all content contained herein, excluding template or example
- *	code, is my own original work.
- */
-
-#include <avr/io.h>
-#ifdef _SIMULATE_
-#include "simAVRHeader.h"
-#endif
-
-unsigned char GetBit(unsigned char x, unsigned char k){
-        return ((x & (0x01 << k)) != 0);
-}
-
-int main(void) {
-	DDRA = 0x00; PORTA = 0x00;
-	DDRB = 0xFF; PORTB = 0x00;
-	DDRC = 0xFF; PORTC = 0x00;
-	unsigned char temp = 0;
-	while(1){
-	    temp = PINA >> 4;
-	    PORTB = temp;
-	    temp = PINA << 4;
-	    PORTC = temp;  
-	} 
-	
-        return 0;
-}
 
 
+
+
+
+tests = [ {'description': 'PINA: 0x63 => PORTB: 0x06 PORTC: 0x30',
+    'steps': [ {'inputs': [('PINA',0x63)], 'iterations': 5 } ],
+    'expected': [('PORTB',0x06),('PORTC',0x30)]
+        },
+
+
+	{'description': 'PINA: 0xAB => PORTB: 0x0A PORTC: 0xB0',
+    'steps': [ {'inputs': [('PINA',0xAB)], 'iterations': 5 } ],
+    'expected': [('PORTB',0x0A),('PORTC',0xB0)]
+        },
+
+
+	{'description': 'PINA: 0x71 => PORTB: 0x07 PORTC: 0x10',
+    'steps': [ {'inputs': [('PINA',0x71)], 'iterations': 5 } ],
+    'expected': [('PORTB',0x07),('PORTC',0x10)]
+        },
+
+  ]
