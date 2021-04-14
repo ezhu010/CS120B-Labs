f880d92a1e2da0a06afc47d5762a73c4d12b663d
Edward Zhu <ezhu010@wch136-24.cs.ucr.edu>
2021-04-13 17:07:49 -0700

    test

--- Lab4_State_Machines/turnin/ezhu010_lab4_part2.c
+++ Lab4_State_Machines/turnin/ezhu010_lab4_part2.c
@@ -1,140 +1,105 @@
-/*	Author: Edward Zhu
- *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab 4  Exercise 1
- *	Exercise Description: [optional - include for your own benefit]
- *
- *	I acknowledge all content contained herein, excluding template or example
- *	code, is my own original work.
- */
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
+enum COUNTER_STATE {COUNTER_START, COUNTER_INIT, COUNTER_INCRE, COUNTER_DECRE, COUNTER_RESET, COUNTER_WAIT} COUNTER_STATE;
 #endif
 
+void COUNTER_SM() {
+	unsigned char A1 = PINA & 0x02;
+	unsigned char A0 = PINA & 0x01;  
 
-enum COUNTER_STATE { COUNTER_BEGIN, COUNTER_INIT, COUNTER_RESET, COUNTER_INCRE, COUNTER_DECRE, COUNTER_WAIT} COUNTER_STATE; 
-
-
-void COUNTER_SM_TICK(){
-	
-	switch(COUNTER_STATE){
-		case COUNTER_BEGIN:
-		     PORTC = 0x07;
-		     if(PINA == 0x01){
-			  PORTC = PORTC + 1;
-		     }
-		     COUNTER_STATE = COUNTER_INIT;
-	   	     break;
-	
-		case COUNTER_INIT:
-	 	     if(PINA == 0x01){
-			    COUNTER_STATE = COUNTER_INCRE;
-		      }
-		     else if( PINA == 0x02){
-			    COUNTER_STATE = COUNTER_DECRE;
-		     }
-		    else if( PINA == 0x03){
-			    COUNTER_STATE = COUNTER_RESET;
-		    }
-		    else {
-			COUNTER_STATE = COUNTER_WAIT; 
-		    }
-		    break;
-
-	      case COUNTER_INCRE:
-		    COUNTER_STATE = COUNTER_WAIT;
-		    break;
-
-	     case COUNTER_DECRE:
-		   COUNTER_STATE = COUNTER_WAIT;
-		   break;
-
-	     case COUNTER_WAIT:
-		   if(PINA == 0x03){
-		      COUNTER_STATE = COUNTER_RESET;
-		   }
-		   else if(PINA == 0x00){
+	switch(COUNTER_STATE){ //Transitions
+		case COUNTER_START: 
 			COUNTER_STATE = COUNTER_INIT;
-		   }
-		   else {
+			break;
+
+		case COUNTER_INIT: 
+			if (A0 && !A1)
+				COUNTER_STATE = COUNTER_INCRE;
+			else if (A1 && !A0)
+				COUNTER_STATE = COUNTER_DECRE;
+			else if (A0 && A1)
+				COUNTER_STATE = COUNTER_RESET;
+			else
+				COUNTER_STATE = COUNTER_WAIT;
+			break;
+
+		case COUNTER_INCRE: 
 			COUNTER_STATE = COUNTER_WAIT;
-		   }
-	            break;
+			break;
 
-
-	    case COUNTER_RESET:
-		   if(PINA == 0x03){
-			COUNTER_STATE = COUNTER_RESET;
-		   }
-
-		   else{
+		case COUNTER_DECRE: 
 			COUNTER_STATE = COUNTER_WAIT;
-		   }
+			break;
 
-		  break;
-	}
+		case COUNTER_WAIT:
+			if (A0 && A1)
+				COUNTER_STATE = COUNTER_RESET;
+			else if (!A0 && !A1)
+				COUNTER_STATE = COUNTER_INIT;
+			else
+				COUNTER_STATE = COUNTER_WAIT;
+			break;
 
-	switch(COUNTER_STATE){
+		case COUNTER_RESET: 
+			if (A0 && A1)
+				COUNTER_STATE = COUNTER_RESET;
+			else
+				COUNTER_STATE = COUNTER_WAIT;
+			break;
 
-		case COUNTER_BEGIN:
-		      PORTC = 0x07;
-		      break;	
-	
-
-	       case COUNTER_INIT:
-		     break;
-
-	       case COUNTER_INCRE:
-		     if(PORTC < 0x09){
-			    PORTC = PORTC + 1;
-		     }
-		     else{
-			    PORTC = 0x09;
-		     }
-		    break;
-	
-	      case COUNTER_DECRE:
-		     if(PORTC > 0x00){
-		          PORTC = PORTC - 1;
-			}
-		      else{
-			 PORTC = 0x00;
-		     }
-
-		    break;
-
-
-
-	     case COUNTER_WAIT:
-		    break;
-
-	    case COUNTER_RESET:
-		    PORTC = 0x00;
-		    break;
+		default: 
+			COUNTER_STATE = COUNTER_START;
+			break;
 
 	}
 
 
+	switch(COUNTER_STATE){ 
+		case COUNTER_START: 
+			PORTC = 0x07;
+			break;
+		
+		case COUNTER_INIT:
+			PORTC = 0x07;
+			break;		
+	
+		case COUNTER_INCRE: 
+			if (PORTC < 0x09)
+				PORTC++;
+			else 
+				PORTC = 0x09;
+			break;
+		case COUNTER_DECRE: 
+			if (PORTC > 0x00)
+				PORTC--;
+			else
+				PORTC = 0x00;
+			break;
+		case COUNTER_WAIT:
+			break;
+		case COUNTER_RESET:
+			PORTC = 0x00;
+			break;
+		default:
+			PORTC = 0x07;
+		break; 
+
+
+	}
 }
-
-
 
 
 int main(void) {
 
-	DDRA = 0x00;
-	PORTA = 0xFF;
-	DDRC = 0xFF;
-	PORTC = 0x00;
-	COUNTER_STATE = COUNTER_BEGIN; 	
+	DDRA = 0x00; PORTA = 0xFF; 
+	DDRC = 0xFF; PORTC = 0x00;
+	COUNTER_STATE = COUNTER_START;
+	    
+	while (1) { 
+		COUNTER_SM();
 
-	while(1){
-
-           COUNTER_SM_TICK();
-
-	}	
-
+	}
 
 	return 1;
 }
