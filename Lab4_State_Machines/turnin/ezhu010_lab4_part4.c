96966c7eecb229213f78208fbd7756bddc1ba792
Edward Zhu <ezhu010@wch136-24.cs.ucr.edu>
2021-04-14 09:36:32 -0700

    test

---
+++ Lab4_State_Machines/turnin/ezhu010_lab4_part4.c
@@ -0,0 +1,165 @@
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+enum DOOR_STATE
+{
+	DOOR_START,
+	DOOR_LOCK,
+	DOOR_UNLOCK_ONE,
+	DOOR_WAIT_ONE,
+	DOOR_UNLOCK_TWO,
+	DOOR_WAIT_TWO,
+	DOOR_LOCK_ONE,
+	DOOR_LOCK_WAIT_ONE,
+} DOOR_STATE;
+
+void DOOR_SM()
+{
+	switch (DOOR_STATE)
+	{
+	case DOOR_START:
+		DOOR_STATE = DOOR_LOCK;
+		break;
+
+	case DOOR_LOCK:
+		if (PINA == 0x04)
+		{
+			DOOR_STATE = DOOR_UNLOCK_ONE;
+		}
+		else
+		{
+			DOOR_STATE = DOOR_LOCK;
+		}
+		break;
+
+	case DOOR_UNLOCK_ONE:
+		if (PINA == 0x04)
+		{
+			DOOR_STATE = DOOR_UNLOCK_ONE;
+		}
+		else if (PINA == 0x00)
+		{
+			DOOR_STATE = DOOR_WAIT_ONE;
+		}
+		else
+		{
+			DOOR_STATE = DOOR_LOCK;
+		}
+		break;
+
+	case DOOR_WAIT_ONE:
+		if (PINA == 0x00)
+		{
+			DOOR_STATE = DOOR_WAIT_ONE;
+		}
+		else if (PINA == 0x02)
+		{
+			DOOR_STATE = DOOR_UNLOCK_TWO;
+		}
+		else
+		{
+			DOOR_STATE = DOOR_LOCK;
+		}
+		break;
+
+	case DOOR_UNLOCK_TWO:
+		if (PINA == 0x02)
+		{
+			DOOR_STATE = DOOR_UNLOCK_TWO;
+		}
+		else if (PINA == 0x00)
+		{
+			DOOR_STATE = DOOR_WAIT_TWO;
+		}
+		break;
+
+	case DOOR_WAIT_TWO:
+		if (PINA == 0x80)
+		{
+			DOOR_STATE = DOOR_LOCK;
+		}
+		else if (PINA == 0x00)
+		{
+			DOOR_STATE = DOOR_WAIT_TWO;
+		}
+		else if (PINA == 0x04)
+		{
+			DOOR_STATE = DOOR_LOCK_ONE;
+		}
+		break;
+
+	case DOOR_LOCK_ONE:
+		if (PINA == 0x04)
+		{
+			DOOR_STATE = DOOR_LOCK_ONE;
+		}
+		else if (PINA == 0x80)
+		{
+			DOOR_STATE = DOOR_LOCK;
+		}
+		else if (PINA == 0x00)
+		{
+			DOOR_STATE = DOOR_LOCK_WAIT_ONE;
+		}
+		break;
+
+	case DOOR_LOCK_WAIT_ONE:
+		if (PINA == 0x00)
+		{
+			DOOR_STATE = DOOR_LOCK_WAIT_ONE;
+		}
+		else if (PINA == 0x02)
+		{
+			DOOR_STATE = DOOR_LOCK;
+		}
+		else if (PINA == 0x80)
+		{
+			DOOR_STATE = DOOR_LOCK;
+		}
+		else
+		{
+			DOOR_STATE = DOOR_WAIT_TWO;
+		}
+	}
+
+	switch (DOOR_STATE)
+	{
+	case DOOR_START:
+		PORTB = 0x00;
+		break;
+	case DOOR_LOCK:
+		PORTB = 0x00;
+		break;
+	case DOOR_UNLOCK_ONE:
+		break;
+	case DOOR_WAIT_ONE:
+		break;
+	case DOOR_UNLOCK_TWO:
+		PORTB = 0x01;
+	case DOOR_WAIT_TWO:
+		break;
+	case DOOR_LOCK_WAIT_ONE:
+		break;
+	case DOOR_LOCK_ONE:
+		break;
+	}
+}
+
+int main(void)
+{
+
+	DDRA = 0x00;
+	PORTA = 0xFF;
+	DDRB = 0xFF;
+	PORTB = 0x00;
+
+	while (1)
+	{
+		DOOR_SM();
+	}
+
+	return 1;
+}
+
