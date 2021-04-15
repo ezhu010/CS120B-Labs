a092dd8154b241c06eaeb0e1f9661405f40abcad
Edward Zhu <ezhu010@wch136-24.cs.ucr.edu>
2021-04-15 08:52:46 -0700

    test

--- Lab4_State_Machines/turnin/ezhu010_lab4_part5.c
+++ Lab4_State_Machines/turnin/ezhu010_lab4_part5.c
@@ -1,13 +1,13 @@
 
 /*	Author: Edward Zhu
- *	 *  Partner(s) Name: 
- *	  *	Lab Section:
- *	   *	Assignment: Lab 4  Exercise 4
- *	    *	Exercise Description: [optional - include for your own benefit]
- *	     *
- *	      *	I acknowledge all content contained herein, excluding template or example
- *	       *	code, is my own original work.
- *	        */
+ *	 *	 *  Partner(s) Name: 
+ *	  *	  *	Lab Section:
+ *	   *	   *	Assignment: Lab 4  Exercise 4
+ *	    *	    *	Exercise Description: [optional - include for your own benefit]
+ *	     *	     *
+ *	      *	      *	I acknowledge all content contained herein, excluding template or example
+ *	       *	       *	code, is my own original work.
+ *	        *	        */
 
 #include <avr/io.h>
 #ifdef _SIMULATE_
@@ -19,7 +19,8 @@ enum DOOR_STATES
     DOOR_START,
     DOOR_LOCK,
     DOOR_UNLOCK,
-    DOOR_PASS,
+    DOOR_BUTTONP,
+    DOOR_WAIT,
 } DOOR_STATE;
 unsigned char count = 0;
 unsigned char arr[4];
@@ -28,118 +29,60 @@ int i = 0;
 void DOOR_SM()
 {
     switch (DOOR_STATE)
-    { 
-    case DOOR_START:
-	if(PINA != 0x00){
-	   DOOR_STATE = DOOR_PASS;
-	}
-	else{
-	  DOOR_STATE = DOOR_LOCK;
-	}
-        break;
-    case DOOR_LOCK:
-        if (PINA != 0x00)
-        {
-            DOOR_STATE = DOOR_PASS;
-        }
-        break;
-    case DOOR_UNLOCK:
-        if (PINA == 0x80)
-        {
-            DOOR_STATE = DOOR_LOCK;
-        }
-        else if (PINA != 0x00)
-        { 
-            DOOR_STATE = DOOR_PASS;
-        }
-        break;
-    case DOOR_PASS:
-        if (count < 4)
-        {
-            DOOR_STATE = DOOR_PASS;
-        }
-
-        else if (PORTB == 0x01)
-        {
-            if (arr[0] == 0x04 && arr[1] == 0x01 && arr[2] == 0x02 && arr[3] == 0x01)
-            {
-                DOOR_STATE = DOOR_LOCK;
-            }
-            else
-            { 
-                DOOR_STATE = DOOR_UNLOCK;
-            }
-        }
-        else if (PORTB == 0x00)
-        {   
-            if (arr[0] == 0x04 && arr[1] == 0x01 && arr[2] == 0x02 && arr[3] == 0x01)
-            {
-                DOOR_STATE = DOOR_UNLOCK;
-            }
-            else
-            {
-                DOOR_STATE = DOOR_LOCK;
-            }
-        }
-        break;
-    default:
-        break;
-    }
-    switch (DOOR_STATE)
     {
     case DOOR_START:
-        break;
-    case DOOR_LOCK:
-
-        PORTB = 0x00;
-        for (i = 0; i < 4; i++)
+        if (PINA != 0x00)
         {
-            arr[i] = 0;
+            arr[count] = PINA;
+            count++;
+            DOOR_STATE = DOOR_BUTTONP;
         }
-        count = 0;
-        break;
-    case DOOR_UNLOCK:
 
-        PORTB = 0x01;
-        for (i = 0; i < 4; i++)
+    case DOOR_BUTTONP:
+        if (PINA != 0)
         {
-            arr[i] = 0;
+            DOOR_STATE = DOOR_BUTTONP;
         }
-        count = 0;
-        break;
-    case DOOR_PASS:
-	if(PINA != 0x00){
-       	 arr[count] = PINA;
-       	 count++;
+	if(count == 4){
+	    PORTB = 0x01;
 	}
+        else
+        {
+            DOOR_STATE = DOOR_WAIT;
+        }
 
-	if (arr[0] == 0x04 && arr[1] == 0x01 && arr[2] == 0x02 && arr[3] == 0x01)
-         {
-		PORTB = 0x01;
-		count = 0;
-		for (i = 0; i < 4; i++)
-       		 {
-            		arr[i] = 0;
-       		 }
-         }
-        break;
-    default:
-        break;
+    case DOOR_WAIT:
+        if (PINA == 0)
+        {
+            DOOR_STATE = DOOR_WAIT;
+        }
+        if (count == 4)
+        {
+            if (arr[0] == 0x04 && arr[1] == 0x01 && arr[2] == 0x02 && arr[3] == 0x01)
+            {
+                PORTB = 0x01;
+            }
+        }
+        else
+        {
+            arr[count] = PINA;
+            count++;
+            DOOR_STATE = DOOR_BUTTONP;
+        }
     }
 }
 
-int main()
+int main(void)
 {
     DDRA = 0x00;
     PORTA = 0xFF;
     DDRB = 0xFF;
     PORTB = 0x00;
-     DOOR_STATE = DOOR_START;
+    DOOR_STATE = DOOR_START;
     while (1)
     {
         DOOR_SM();
     }
 }
-
 
 
