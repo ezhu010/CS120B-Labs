b72faa209bba48e681866fedbda58a9f2a872d61
Edward Zhu <ezhu010@wch136-24.cs.ucr.edu>
2021-04-15 16:50:40 -0700

    test

--- Lab4_State_Machines/turnin/ezhu010_lab4_part5.c
+++ Lab4_State_Machines/turnin/ezhu010_lab4_part5.c
@@ -1,51 +1,52 @@
 
 
-/*	Author: Edward Zhu
- *	 *	 *	 *  Partner(s) Name: 
- *	  *	  *	  *	Lab Section:
- *	   *	   *	   *	Assignment: Lab 4  Exercise 4
- *	    *	    *	    *	Exercise Description: [optional - include for your own benefit]
- *	     *	     *	     *
- *	      *	      *	      *	I acknowledge all content contained herein, excluding template or example
- *	       *	       *	       *	code, is my own original work.
- *	        *	        *	        */
 
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
 
+int count = 0;
+int arr[4];
+
 enum DOOR_STATES
 {
     DOOR_START,
-    BUTTON_PRESS,
+    BUTTON_PRESSED,
     DOOR_WAIT,
+    DOOR_UNLOCK,
+    DOOR_LOCK,
 } DOOR_STATE;
-unsigned char count = 0;
-unsigned char arr[4] = {0,0,0,0};
-int i = 0;
 
 void DOOR_SM()
 {
     switch (DOOR_STATE)
     {
     case DOOR_START:
-        if (PINA != 0)
+        if (PINA == 0x00)
         {
-            arr[count++] = PINA;
-            DOOR_STATE = BUTTON_PRESS;
-        }
-        break;
-    case BUTTON_PRESS:
-        if (PINA != 0)
-        {
-            DOOR_STATE = BUTTON_PRESS;
+            DOOR_STATE = DOOR_LOCK;
         }
         else
         {
-            DOOR_STATE = DOOR_WAIT;
+            arr[count] = PINA;
+            count++;
+            DOOR_STATE = BUTTON_PRESSED;
         }
         break;
+
+    case BUTTON_PRESSED:
+        if (PINA != 0)
+        {
+            DOOR_STATE = BUTTON_PRESSED;
+        }
+        else if (PINA == 0)
+        {
+            DOOR_STATE = DOOR_WAIT;
+        }
+
+        break;
+
     case DOOR_WAIT:
         if (PINA == 0)
         {
@@ -53,27 +54,54 @@ void DOOR_SM()
         }
         else
         {
-            arr[count++] = PINA;
+            arr[count] = PINA;
+            count++;
             if (count == 4)
             {
                 if (arr[0] == 0x04 && arr[1] == 0x01 && arr[2] == 0x02 && arr[3] == 0x01)
                 {
-                    PORTB = 1;
+                    DOOR_STATE = DOOR_UNLOCK;
                 }
                 else
                 {
-                    PORTB = 0;
+                    DOOR_STATE = DOOR_LOCK;
                 }
             }
-            DOOR_STATE = BUTTON_PRESS;
+            else
+            {
+                DOOR_STATE = BUTTON_PRESSED;
+            }
         }
+        break;
+
+    case DOOR_UNLOCK:
+        break;
+
+    case DOOR_LOCK:
+        if (PINA == 0x80)
+        {
+            DOOR_STATE = DOOR_LOCK;
+        }
+        else if (PINA == 0)
+        {
+            DOOR_STATE = DOOR_LOCK;
+        }
+        break;
     }
 
     switch (DOOR_STATE)
     {
-    case BUTTON_PRESS:
+    case DOOR_START:
         break;
-    default:
+    case DOOR_LOCK:
+        PORTB = 0x00;
+        break;
+    case BUTTON_PRESSED:
+        break;
+    case DOOR_UNLOCK:
+        PORTB = 0x01;
+        break;
+    case DOOR_WAIT:
         break;
     }
 }
@@ -82,20 +110,14 @@ int main(void)
 {
     DDRA = 0x00;
     PORTA = 0xFF;
-    DDRB = 0xFF;
+    DDRB = 0x0FF;
     PORTB = 0x00;
     DOOR_STATE = DOOR_START;
+
     while (1)
     {
         DOOR_SM();
     }
+    return 1;
 }
-
-//{'inputs' : [('PINA', 0x00)], 'iterations' : 3},
-//// {'inputs' : [('PINA', 0x01)], 'iterations' : 2},
-//// {'inputs' : [('PINA', 0x00)], 'iterations' : 3},
-//// {'inputs' : [('PINA', 0x02)], 'iterations' : 2},
-//// {'inputs' : [('PINA', 0x00)], 'iterations' : 2},
-//// {'inputs' : [('PINA', 0x01)], 'iterations' : 2},
-//
 
