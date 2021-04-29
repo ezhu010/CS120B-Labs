599cc496688d9ff0d03e1847ad1c58608614da12
Edward <ezhu010@ucr.edu>
2021-04-29 09:41:25 -0700

    test

--- Lab7/turnin/ezhu010_lab7_part3.c
+++ Lab7/turnin/ezhu010_lab7_part3.c
@@ -5,7 +5,7 @@
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
-unsigned short x = 0x00;
+unsigned short my_short = 0x00;
 unsigned short max = 200;
 
 void ADC_init() {
@@ -18,8 +18,8 @@ int main(void) {
     DDRD = 0xFF; PORTD = 0x00;
     ADC_init();
     while (1) {
-	x = ~ADC;
-	unsigned char temp = x;
+	my_short = ADC;
+	unsigned char temp = my_short;
 	if (temp >= max/2) {
 		PORTB = 0x01;
 	}
