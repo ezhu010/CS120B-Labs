faf1414e4a24e687ddb3d7fd3624f3218d14de38
ezhu010 <ezhu010@ucr.edu>
2021-05-03 08:36:02 -0700

    test

--- Lab8/source/main.c
+++ Lab8/source/main.c
@@ -8,28 +8,46 @@
  *	code, is my own original work.
  */
 #include <avr/io.h>
+#include "io.h"
+#include <avr/interrupt.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
-
+#include "timer.h"
+enum States
+{
+    Start,
+    Init,
+    Cnote,
+    Dnote,
+    Enote
+} state;
+double temp = 0;
 void set_PWM(double frequency)
 {
     static double current_frequency;
-
     if (frequency != current_frequency)
     {
         if (!frequency)
+        {
             TCCR3B &= 0x08;
+        }
         else
+        {
             TCCR3B |= 0x03;
-
+        }
         if (frequency < 0.954)
+        {
             OCR3A = 0xFFFF;
+        }
         else if (frequency > 31250)
+        {
             OCR3A = 0x0000;
+        }
         else
+        {
             OCR3A = (short)(8000000 / (128 * frequency)) - 1;
-
+        }
         TCNT3 = 0;
         current_frequency = frequency;
     }
@@ -48,13 +66,106 @@ void PWM_off()
     TCCR3B = 0x00;
 }
 
-int main(void)
+void Tick()
 {
-    /* Insert DDR and PORT initializations */
+    switch (state)
+    {
+    case Start:
+        state = Init;
+        break;
 
-    /* Insert your solution below */
+    case Init:
+        if ((~PINA & 0x07) == 0x01)
+        {
+            state = Cnote;
+        }
+        else if ((~PINA & 0x07) == 0x02)
+        {
+            state = Dnote;
+        }
+        else if ((~PINA & 0x07) == 0x04)
+        {
+            state = Enote;
+        }
+        else
+        {
+            state = Init;
+        }
+        break;
+
+    case Cnote:
+        if ((~PINA & 0x07) == 0x01)
+        {
+            state = Cnote;
+        }
+        else
+        {
+            state = Init;
+        }
+        break;
+
+    case Dnote:
+        if ((~PINA & 0x07) == 0x02)
+        {
+            state = Dnote;
+        }
+        else
+        {
+            state = Init;
+        }
+        break;
+
+    case Enote:
+        if ((~PINA & 0x07) == 0x04)
+        {
+            state = Enote;
+        }
+        else
+        {
+            state = Init;
+        }
+        break;
+
+    default:
+        state = Start;
+        break;
+    }
+    switch (state)
+    {
+    case Start:
+        break;
+    case Init:
+        set_PWM(0);
+        break;
+    case Cnote:
+        set_PWM(261.63);
+        break;
+    case Dnote:
+        set_PWM(293.66);
+        break;
+    case Enote:
+        set_PWM(329.63);
+        break;
+    default:
+        break;
+    }
+}
+
+void main(void)
+{
+    DDRA = 0x00;
+    PORTA = 0xFF;
+    DDRB = 0xFF;
+    PORTB = 0x00;
+    TimerSet(100);
+    TimerOn();
+    PWM_on();
     while (1)
     {
+        Tick();
+        while (!TimerFlag)
+        {
+        };
+        TimerFlag = 0;
     }
-    return 1;
 }
