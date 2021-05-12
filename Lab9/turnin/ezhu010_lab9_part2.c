19416e89df03c754a2374d565b016508db1d8f31
Edward <ezhu010@ucr.edu>
2021-05-12 07:40:47 -0700

    test

---
+++ Lab9/turnin/ezhu010_lab9_part2.c
@@ -0,0 +1,158 @@
+
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+#include "timer.h"
+enum ThreeLEDStates
+{
+	Zero,
+	One,
+	Two
+} ThreeLEDstate;
+enum BlinkingLEDStates
+{
+	BlinkingStart,
+	BlinkingInit,
+	Blink
+} BlinkingLEDstate;
+enum CombineLEDStates
+{
+	CombineStart,
+	CombineInit
+} CombineLEDstate;
+unsigned char threeLEDs = 0x00;
+unsigned char blinkingLED = 0x00;
+
+void TickThreeLEDsSM()
+{
+	switch (ThreeLEDstate)
+	{
+
+	case Zero:
+		ThreeLEDstate = One;
+		break;
+
+	case One:
+		ThreeLEDstate = Two;
+		break;
+
+	case Two:
+		ThreeLEDstate = Zero;
+		break;
+	}
+
+	switch (ThreeLEDstate)
+	{
+
+	case Zero:
+		threeLEDs = 0x01;
+		break;
+	case One:
+		threeLEDs = 0x02;
+		break;
+	case Two:
+		threeLEDs = 0x04;
+		break;
+	default:
+		break;
+	}
+}
+
+void TickBlinkingLEDSM()
+{
+	switch (BlinkingLEDstate)
+	{
+	case BlinkingStart:
+		BlinkingLEDstate = BlinkingInit;
+		break;
+	case BlinkingInit:
+		BlinkingLEDstate = Blink;
+		break;
+	case Blink:
+		BlinkingLEDstate = BlinkingInit;
+		break;
+	default:
+		BlinkingLEDstate = BlinkingStart;
+		break;
+	}
+	switch (BlinkingLEDstate)
+	{
+	case BlinkingStart:
+		break;
+	case BlinkingInit:
+		blinkingLED = 0x00;
+		break;
+	case Blink:
+		blinkingLED = 0x01;
+		break;
+	default:
+		break;
+	}
+}
+
+void TickCombineLEDsSM()
+{
+	switch (CombineLEDstate)
+	{
+	case CombineStart:
+		CombineLEDstate = CombineInit;
+		break;
+	case CombineInit:
+		CombineLEDstate = CombineInit;
+		break;
+	default:
+		CombineLEDstate = CombineStart;
+		break;
+	}
+	switch (CombineLEDstate)
+	{
+	case CombineStart:
+		break;
+	case CombineInit:
+		PORTB = ((blinkingLED << 3) | (threeLEDs));
+		break;
+	default:
+		break;
+	}
+}
+
+int main(void)
+{
+	DDRA = 0x00;
+	PORTA = 0xFF;
+	DDRB = 0xFF;
+	PORTB = 0x00;
+	unsigned long ThreeElapsed = 0;
+	unsigned long BlinkingElapsed = 0;
+	const unsigned long timerPeriod = 100;
+	TimerSet(100);
+	TimerOn();
+	ThreeLEDstate = Zero;
+	BlinkingLEDstate = BlinkingStart;
+	CombineLEDstate = CombineStart;
+
+	while (1)
+	{
+		if (ThreeElapsed >= 300)
+		{
+			TickThreeLEDsSM();
+			ThreeElapsed = 0;
+		}
+		if (BlinkingElapsed >= 1000)
+		{
+			TickBlinkingLEDSM();
+			BlinkingElapsed = 0;
+		}
+		TickCombineLEDsSM();
+		while (!TimerFlag)
+		{
+		};
+		TimerFlag = 0;
+		ThreeElapsed += timerPeriod;
+		BlinkingElapsed += timerPeriod;
+	}
+	return 0;
+}
