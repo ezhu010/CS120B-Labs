1d2f2376185ae15c511273254b10275bbae047b8
ezhu010 <ezhu010@ucr.edu>
2021-05-17 17:42:39 -0700

    test

--- Lab10/source/main.c
+++ Lab10/source/main.c
@@ -1,189 +1,141 @@
-
 #include <avr/io.h>
+#include <timer.h>
+#include <keypad.h>
+#include <io.h>
+#include <scheduler.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
-#include "timer.h"
 
-typedef struct task
+unsigned char x = 0x00;
+unsigned char keypad = 0x00;
+
+enum KeypadStates
 {
-    signed char state;
-    unsigned long int period;
-    unsigned long int elaspedTime;
-    int (*TickFct)(int);
-} task;
-
-unsigned char led0_output = 0;
-unsigned char led1_output = 0;
-
-unsigned char pause = 0;
-
-enum pauseButtonSM_States
-{
-    pauseButton_wait,
-    pauseButton_press,
-    pauseButton_release
+    Start,
+    Init
 };
-
-int pauseButtonSMTick(int state)
+int KeypadTick(int state)
 {
-    unsigned char press = ~PINA & 0x01;
+    x = GetKeypadKey();
     switch (state)
     {
-    case pauseButton_wait:
-        state = press == 0x01 ? pauseButton_press : pauseButton_wait;
+    case Start:
+        state = Init;
         break;
-    case pauseButton_press:
-        state = pauseButton_release;
+    case Init:
+        state = Init;
         break;
-    case pauseButton_release:
-        state = press == 0x00 ? pauseButton_wait : pauseButton_press;
+    default:
+        state = Start;
         break;
     }
     switch (state)
     {
-    case pauseButton_wait:
+    case Start:
         break;
-    case pauseButton_press:
-        pause = (pause == 0) ? 1 : 0;
+    case Init:
+        switch (x)
+        {
+        case '\0':
+            keypad = 0x1F;
+            break;
+        case '1':
+            keypad = 0x01;
+            break;
+        case '2':
+            keypad = 0x02;
+            break;
+        case '3':
+            keypad = 0x03;
+            break;
+        case '4':
+            keypad = 0x04;
+            break;
+        case '5':
+            keypad = 0x05;
+            break;
+        case '6':
+            keypad = 0x06;
+            break;
+        case '7':
+            keypad = 0x07;
+            break;
+        case '8':
+            keypad = 0x08;
+            break;
+        case '9':
+            keypad = 0x09;
+            break;
+        case 'A':
+            keypad = 0x0A;
+            break;
+        case 'B':
+            keypad = 0x0B;
+            break;
+        case 'C':
+            keypad = 0x0C;
+            break;
+        case 'D':
+            keypad = 0x0D;
+            break;
+        case '*':
+            keypad = 0x0E;
+            break;
+        case '0':
+            keypad = 0x00;
+            break;
+        case '#':
+            keypad = 0x0F;
+            break;
+        default:
+            keypad = 0x1B;
+            break;
+        }
+        PORTB = keypad;
         break;
-    case pauseButton_release:
-        break;
-    }
-    return state;
-}
 
-enum toggleLED0_States
-{
-    toggleLED0_wait,
-    toggleLED0_blink
-};
-
-int toggleLED0SMTick(int state)
-{
-    switch (state)
-    {
-    case toggleLED0_wait:
-        state = !pause ? toggleLED0_blink : toggleLED0_wait;
-        break;
-    case toggleLED0_blink:
-        state = pause ? toggleLED0_wait : toggleLED0_blink;
+    default:
         break;
     }
-    switch (state)
-    {
-    case toggleLED0_wait:
-        break;
-    case toggleLED0_blink:
-        led0_output = (led0_output == 0x00) ? 0x01 : 0x00;
-        break;
-    }
-    return state;
-}
-
-enum toggleLED1_States
-{
-    toggleLED1_wait,
-    toggleLED1_blink
-};
-
-int toggleLED1SMTick(int state)
-{
-    switch (state)
-    {
-    case toggleLED1_wait:
-        state = !pause ? toggleLED1_blink : toggleLED1_wait;
-        break;
-    case toggleLED1_blink:
-        state = pause ? toggleLED1_wait : toggleLED1_blink;
-        break;
-    }
-    switch (state)
-    {
-    case toggleLED1_wait:
-        break;
-    case toggleLED1_blink:
-        led1_output = (led1_output == 0x00) ? 0x01 : 0x00;
-        break;
-    }
-    return state;
-}
-
-enum displayStates
-{
-    display_display
-};
-
-int displaySMTick(int state)
-{
-    unsigned char output;
-    switch (state)
-    {
-    case display_display:
-        state = display_display;
-        break;
-    }
-    switch (state)
-    {
-    case display_display:
-        output = led0_output | led1_output << 1;
-        break;
-    }
-    PORTB = output;
     return state;
 }
 
 int main(void)
 {
-    DDRA = 0x00;
-    PORTA = 0xFF;
+    /* Insert DDR and PORT initializations */
     DDRB = 0xFF;
     PORTB = 0x00;
-
-    static task task1, task2, task3, task4;
-    task *tasks[] = {&task1, &task2, &task3, &task4};
+    DDRC = 0xF0;
+    PORTC = 0x0F;
+    /* Insert your solution below */
+    static task task1;
+    task *tasks[] = {&task1};
     const unsigned short numTasks = sizeof(tasks) / sizeof(task *);
-
-    const char start = -1;
+    const char start = 0;
     task1.state = start;
     task1.period = 50;
-    task1.elaspedTime = task1.period;
-    task1.TickFct = &pauseButtonSMTick;
+    task1.elapsedTime = task1.period;
+    task1.TickFct = &KeypadTick;
 
-    task2.state = start;
-    task2.period = 500;
-    task2.elaspedTime = task2.period;
-    task2.TickFct = &toggleLED0SMTick;
-
-    task3.state = start;
-    task3.period = 1000;
-    task3.elaspedTime = task3.period;
-    task3.TickFct = &toggleLED1SMTick;
-
-    task4.state = start;
-    task4.period = 10;
-    task4.elaspedTime = task4.period;
-    task4.TickFct = &displaySMTick;
-
-    TimerSet(10);
+    TimerSet(50);
     TimerOn();
+
     unsigned short i;
     while (1)
     {
-        PORTB = 0xFF;
-        // for (i = 0; i < numTasks; i++)
-        // {
-        //     if (tasks[i]->elaspedTime == tasks[i]->period)
-        //     {
-        //         tasks[i]->state = tasks[i]->TickFct(tasks[i]->state);
-        //         tasks[i]->elaspedTime = 0;
-        //     }
-        //     tasks[i]->elaspedTime += 10;
-        // }
-        // while (!TimerFlag)
-        // {
-        // };
-        // TimerFlag = 0;
+        for (i = 0; i < numTasks; ++i)
+        {
+            if (tasks[i]->elapsedTime == tasks[i]->period)
+            {
+                tasks[i]->state = tasks[i]->TickFct(tasks[i]->state);
+                tasks[i]->elapsedTime = 0;
+            }
+            tasks[i]->elapsedTime += 50;
+        }
+        while (!TimerFlag)
+            ;
+        TimerFlag = 0;
     }
-    return 0;
+    return 1;
 }
