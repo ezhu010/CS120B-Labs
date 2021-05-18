12bc7f7f6197b664d72038719c614b7f0b1f241b
ezhu010 <ezhu010@ucr.edu>
2021-05-17 17:46:56 -0700

    test

--- Lab10/source/main.c
+++ Lab10/source/main.c
@@ -1,11 +1,91 @@
 #include <avr/io.h>
 #include <timer.h>
-#include <keypad.h>
-#include <io.h>
-#include <scheduler.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
+
+unsigned char GetBit(unsigned char x, unsigned char k)
+{
+    return ((x & (0x01 << k)) != 0);
+}
+
+unsigned char GetKeypadKey()
+{
+    PORTC = 0xEF;
+    asm("nop");
+    if (GetBit(PINC, 0) == 0)
+    {
+        return ('1');
+    }
+    if (GetBit(PINC, 1) == 0)
+    {
+        return ('4');
+    }
+    if (GetBit(PINC, 2) == 0)
+    {
+        return ('7');
+    }
+    if (GetBit(PINC, 3) == 0)
+    {
+        return ('*');
+    }
+
+    PORTC = 0xDF;
+    asm("nop");
+    if (GetBit(PINC, 0) == 0)
+    {
+        return ('2');
+    }
+    if (GetBit(PINC, 1) == 0)
+    {
+        return ('5');
+    }
+    if (GetBit(PINC, 2) == 0)
+    {
+        return ('8');
+    }
+    if (GetBit(PINC, 3) == 0)
+    {
+        return ('0');
+    }
+
+    PORTC = 0xBF;
+    asm("nop");
+    if (GetBit(PINC, 0) == 0)
+    {
+        return ('3');
+    }
+    if (GetBit(PINC, 1) == 0)
+    {
+        return ('6');
+    }
+    if (GetBit(PINC, 2) == 0)
+    {
+        return ('9');
+    }
+    if (GetBit(PINC, 3) == 0)
+    {
+        return ('#');
+    }
+    PORTC = 0x7F;
+    if (GetBit(PINC, 0) == 0)
+    {
+        return ('A');
+    }
+    if (GetBit(PINC, 1) == 0)
+    {
+        return ('B');
+    }
+    if (GetBit(PINC, 2) == 0)
+    {
+        return ('C');
+    }
+    if (GetBit(PINC, 3) == 0)
+    {
+        return ('D');
+    }
+    return ('\0');
+}
 
 unsigned char x = 0x00;
 unsigned char keypad = 0x00;
