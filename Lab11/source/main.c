e35c8c538d397c088e04cdd90bfc366deee4998f
ezhu010 <ezhu010@ucr.edu>
2021-05-24 13:38:49 -0700

    test

--- Lab11/source/main.c
+++ Lab11/source/main.c
@@ -1,23 +1,98 @@
-/*	Author: lab
- *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
- *	Exercise Description: [optional - include for your own benefit]
- *
- *	I acknowledge all content contained herein, excluding template or example
- *	code, is my own original work.
- */
+
 #include <avr/io.h>
+#include "timer.h"
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
 
-int main(void) {
-    /* Insert DDR and PORT initializations */
+typedef struct task
+{
+    signed char state;
+    unsigned long period;
+    unsigned long elapsedTime; //Time elapsed since last task tick
+    int (*TickFct)(int);       //Task tick function
+} task;
 
-    /* Insert your solution below */
-    while (1) {
+enum Demo_States
+{
+    shift
+};
+int Demo_Tick(int state)
+{
 
+    // Local Variables
+    static unsigned char pattern = 0x80; // LED pattern - 0: LED off; 1: LED on
+    static unsigned char row = 0xFE;     // Row(s) displaying pattern.
+                                         // 0: display pattern on row
+                                         // 1: do NOT display pattern on row
+    // Transitions
+    switch (state)
+    {
+    case shift:
+        break;
+    default:
+        state = shift;
+        break;
     }
-    return 1;
+    // Actions
+    switch (state)
+    {
+    case shift:
+        if (row == 0xEF && pattern == 0x01)
+        { // Reset demo
+            pattern = 0x80;
+            row = 0xFE;
+        }
+        else if (pattern == 0x01)
+        { // Move LED to start of next row
+            pattern = 0x80;
+            row = (row << 1) | 0x01;
+        }
+        else
+        { // Shift LED one spot to the right on current row
+            pattern >>= 1;
+        }
+        break;
+    default:
+        break;
+    }
+    PORTC = pattern; // Pattern to display
+    PORTD = row;     // Row(s) displaying pattern
+    return state;
+}
+
+int main(void)
+{
+    DDRD = 0xFF;
+    PORTD = 0x00;
+    DDRC = 0xFF;
+    PORTC = 0x00;
+    static task task1;
+    task *tasks[] = {&task1};
+    const unsigned short numTasks = sizeof(tasks) / sizeof(task *);
+    const char start = 0;
+    task1.state = start;
+    task1.period = 100;
+    task1.elapsedTime = task1.period;
+    task1.TickFct = &Demo_Tick;
+
+    TimerSet(100);
+    TimerOn();
+    unsigned short i;
+    while (1)
+    {
+        // for (i = 0; i < numTasks; ++i)
+        // {
+        //     if (tasks[i]->elapsedTime == tasks[i]->period)
+        //     {
+        //         tasks[i]->state = tasks[i]->TickFct(tasks[i]->state);
+        //         tasks[i]->elapsedTime = 0;
+        //     }
+        //     tasks[i]->elapsedTime += 100;
+        // }
+        // while (!TimerFlag)
+        // {
+        // };
+        // TimerFlag = 0;
+    }
 }
