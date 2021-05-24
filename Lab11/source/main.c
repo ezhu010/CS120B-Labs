1dc5d9960f0bf17356b0ad1947df6861e8d61d1c
ezhu010 <ezhu010@ucr.edu>
2021-05-24 15:32:42 -0700

    test

--- Lab11/source/main.c
+++ Lab11/source/main.c
@@ -81,18 +81,21 @@ int main(void)
     unsigned short i;
     while (1)
     {
-        for (i = 0; i < numTasks; ++i)
-        {
-            if (tasks[i]->elapsedTime == tasks[i]->period)
-            {
-                tasks[i]->state = tasks[i]->TickFct(tasks[i]->state);
-                tasks[i]->elapsedTime = 0;
-            }
-            tasks[i]->elapsedTime += 100;
-        }
-        while (!TimerFlag)
-        {
-        };
-        TimerFlag = 0;
+
+        PORTC = 0xFF;
+        PORTD = 0x16;
+        // for (i = 0; i < numTasks; ++i)
+        // {
+        //     if (tasks[i]->elapsedTime == tasks[i]->period)
+        //     {
+        //         tasks[i]->state = tasks[i]->TickFct(tasks[i]->state);
+        //         tasks[i]->elapsedTime = 0;
+        //     }
+        //     tasks[i]->elapsedTime += 100;
+        // }
+        // while (!TimerFlag)
+        // {
+        // };
+        // TimerFlag = 0;
     }
 }
