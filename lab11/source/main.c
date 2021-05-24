60a751c5a2382080b1f8ba67f118c0a5f9d47732
ezhu010 <ezhu010@ucr.edu>
2021-05-24 13:25:24 -0700

    test

--- lab11/source/main.c
+++ lab11/source/main.c
@@ -1,23 +1,51 @@
-/*	Author: lab
- *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
- *	Exercise Description: [optional - include for your own benefit]
- *
- *	I acknowledge all content contained herein, excluding template or example
- *	code, is my own original work.
- */
-#include <avr/io.h>
-#ifdef _SIMULATE_
-#include "simAVRHeader.h"
-#endif
+//--------------------------------------
+// LED Matrix Demo SynchSM
+// Period: 100 ms
+//--------------------------------------
+enum Demo_States
+{
+    shift
+};
+int Demo_Tick(int state)
+{
 
-int main(void) {
-    /* Insert DDR and PORT initializations */
-
-    /* Insert your solution below */
-    while (1) {
-
+    // Local Variables
+    static unsigned char pattern = 0x80; // LED pattern - 0: LED off; 1: LED on
+    static unsigned char row = 0xFE;     // Row(s) displaying pattern.
+                                         // 0: display pattern on row
+                                         // 1: do NOT display pattern on row
+    // Transitions
+    switch (state)
+    {
+    case shift:
+        break;
+    default:
+        state = shift;
+        break;
     }
-    return 1;
+    // Actions
+    switch (state)
+    {
+    case shift:
+        if (row == 0xEF && pattern == 0x01)
+        { // Reset demo
+            pattern = 0x80;
+            row = 0xFE;
+        }
+        else if (pattern == 0x01)
+        { // Move LED to start of next row
+            pattern = 0x80;
+            row = (row << 1) | 0x01;
+        }
+        else
+        { // Shift LED one spot to the right on current row
+            pattern >>= 1;
+        }
+        break;
+    default:
+        break;
+    }
+    PORTC = pattern; // Pattern to display
+    PORTD = row;     // Row(s) displaying pattern
+    return state;
 }
