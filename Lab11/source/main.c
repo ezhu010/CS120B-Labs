13aefbc57d228f93b3c6dfaa9ccb9ab6888633ff
ezhu010 <ezhu010@ucr.edu>
2021-05-25 15:46:53 -0700

    test

--- Lab11/source/main.c
+++ Lab11/source/main.c
@@ -74,7 +74,7 @@ int main(void)
     PORTD = 0x00;
     DDRC = 0xFF;
     PORTC = 0x00;
-    static task task1;
+    static task task1, task2;
     task *tasks[] = {&task1, &task2};
     const unsigned short numTasks = sizeof(tasks) / sizeof(task *);
     const char start = 0;
