a4d3c592fca6562e1a59e15d411cb633f9a20966
ezhu010 <ezhu010@ucr.edu>
2021-05-25 10:22:32 -0700

    test

--- Lab11/source/main.c
+++ Lab11/source/main.c
@@ -82,7 +82,7 @@ int main(void)
     unsigned short i;
     while (1)
     {
-        PORTD = 0xFD;
+        PORTD = 0x0F;
         PORTC = 0x80;
         //     for (i = 0; i < numTasks; ++i)
         //     {
