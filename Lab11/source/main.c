af0a6a845039b9cab1c911bfb788d4f1b2c71126
ezhu010 <ezhu010@ucr.edu>
2021-05-25 15:46:04 -0700

    test

--- Lab11/source/main.c
+++ Lab11/source/main.c
@@ -15,6 +15,22 @@ typedef struct task
     int (*TickFct)(int);       //Task tick function
 } task;
 
+enum Random_States
+{
+    random_init
+};
+int temp = 0;
+int random_tick(int state)
+{
+    switch (state)
+    {
+    case random_init:
+        temp = rand() % 255 + 1;
+        break;
+    }
+    return state;
+}
+
 enum Demo_States
 {
     shift
@@ -22,9 +38,7 @@ enum Demo_States
 int LED_MATRIX(int state)
 {
     static unsigned char column = 0x1E;
-    int temp = rand() % 99 + 1;
-     unsigned char row = temp;
-
+    unsigned char row = temp;
     switch (state)
     {
     case shift:
@@ -55,18 +69,24 @@ int LED_MATRIX(int state)
 
 int main(void)
 {
+    srand(time(0));
     DDRD = 0xFF;
     PORTD = 0x00;
     DDRC = 0xFF;
     PORTC = 0x00;
     static task task1;
-    task *tasks[] = {&task1};
+    task *tasks[] = {&task1, &task2};
     const unsigned short numTasks = sizeof(tasks) / sizeof(task *);
     const char start = 0;
     task1.state = start;
     task1.period = 300;
     task1.elapsedTime = task1.period;
     task1.TickFct = &LED_MATRIX;
+
+    task2.state = start;
+    task2.period = 1200;
+    task2.elapsedTime = task2.period;
+    task2.TickFct = &random_tick;
 
     TimerSet(300);
     TimerOn();
