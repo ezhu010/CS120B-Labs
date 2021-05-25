62aea17492a38b5a4f726472fccbddc4b058a14a
ezhu010 <ezhu010@ucr.edu>
2021-05-25 10:05:30 -0700

    test

--- Lab11/source/main.c
+++ Lab11/source/main.c
@@ -72,7 +72,7 @@ int main(void)
     const unsigned short numTasks = sizeof(tasks) / sizeof(task *);
     const char start = 0;
     task1.state = start;
-    task1.period = 100;
+    task1.period = 200;
     task1.elapsedTime = task1.period;
     task1.TickFct = &Demo_Tick;
 
