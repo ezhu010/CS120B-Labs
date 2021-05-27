208c5045148cadabdc6b07dd0f95fc8b12f81956
ezhu010 <ezhu010@ucr.edu>
2021-05-26 17:40:31 -0700

    test

--- Lab11/source/main.c
+++ Lab11/source/main.c
@@ -131,7 +131,7 @@ int main(void)
                 tasks[i]->state = tasks[i]->TickFct(tasks[i]->state);
                 tasks[i]->elapsedTime = 0;
             }
-            tasks[i]->elapsedTime += 1;
+            tasks[i]->elapsedTime += 50;
         }
         while (!TimerFlag)
         {
