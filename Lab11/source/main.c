ac90fd3f5edd28c77fcd8334cfdfd974401c1de7
ezhu010 <ezhu010@ucr.edu>
2021-05-26 17:55:25 -0700

    test

--- Lab11/source/main.c
+++ Lab11/source/main.c
@@ -84,7 +84,7 @@ int PLAYER_SM(int state)
     switch (state)
     {
     case PLAYER_INIT:
-        if (counter2 == 10)
+        if (counter2 == 100)
         {
             PORTC = 0x10;
             PORTD = 0x0F; // 0000 1111
