fe5d4e771e0d1f6a4e87ed038cb29af6b093d338
ezhu010 <ezhu010@ucr.edu>
2021-05-26 17:49:08 -0700

    test

--- Lab11/source/main.c
+++ Lab11/source/main.c
@@ -55,7 +55,7 @@ int LED_MATRIX(int state)
         {
             column = 0x1E;
         }
-        else if (count == 5)
+        else if (count == 100)
         {
             column = ((column << 1) + 1);
             count = 0;
