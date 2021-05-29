b7e989f3817d48f7fa9ecedb74a1a9f1b213d5df
ezhu010 <ezhu010@ucr.edu>
2021-05-29 16:00:25 -0700

    test

--- Lab11/source/main.c
+++ Lab11/source/main.c
@@ -218,7 +218,7 @@ int main(void)
         }
         if (totalTimeElapsed == 6000)
         {
-            task2.period = 5;
+            task2.period = 3;
             newTimer = 180;
         }
         totalTimeElapsed += 1;
