040f14ef9aa81c7e1da02c1d416fc33db8ab3007
ezhu010 <ezhu010@ucr.edu>
2021-05-29 15:03:21 -0700

    test

--- Lab11/source/main.c
+++ Lab11/source/main.c
@@ -204,11 +204,11 @@ int main(void)
                 tasks[i]->state = tasks[i]->TickFct(tasks[i]->state);
                 tasks[i]->elapsedTime = 0;
             }
-            // if (totalTimeElapsed == 5000) // speed up the game after __ seconds
-            // {
-            //     task1.period = 1250;
-            //     led_ticker = 50;
-            // }
+            if (totalTimeElapsed == 3000) // speed up the game after __ seconds
+            {
+                task1.period = 1300;
+                led_ticker = 50;
+            }
             tasks[i]->elapsedTime += 1;
             totalTimeElapsed += 1;
         }
